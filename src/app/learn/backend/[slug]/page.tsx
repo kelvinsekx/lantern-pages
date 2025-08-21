977e726d4897856f29b1ac7d2a74e3f0e3e984d4
kelvinsekx <utdkelvin@gmail.com>
2025-08-21 16:34:04 +0100

    Add new components and update BlogPage layout

--- src/app/learn/backend/[slug]/page.tsx
+++ src/app/learn/backend/[slug]/page.tsx
@@ -1,7 +1,10 @@
 import path from "node:path";
-import { MDXRemote } from "next-mdx-remote-client/rsc";
+import { MDXRemote, type MDXRemoteOptions } from "next-mdx-remote-client/rsc";
 import { readMDXFile } from "@/utilities/mdx-utils";
+import { plugins } from "@/utilities/mdx-plugins";
+
 import { notFound } from "next/navigation";
+import { AsideNote } from "@/components/aside-note";
 
 type Params = Promise<{ slug: string }>;
 
@@ -9,7 +12,7 @@ export default async function Page(props: { params: Params }) {
   const params = await props.params;
   const slug = params.slug;
 
-  const { metadata, content } = await readMDXFile(
+  const { content } = await readMDXFile(
     `${path.join(
       process.cwd(),
       "src",
@@ -17,18 +20,36 @@ export default async function Page(props: { params: Params }) {
       "backend",
       slug,
       "contents",
-      "001-introduction-to-development.mdx"
+      "000-welcome.mdx"
     )}`
   );
+
+  const options: MDXRemoteOptions = {
+    disableImports: true, // import statements in MDX don't work in pages router
+    parseFrontmatter: true,
+    scope: {
+      // readingTime: readingTime(source, 100).text,
+      props: { foo: "props in scope is working" },
+    },
+    // vfileDataIntoScope: "toc", // the "remark-flexible-toc" plugin produces vfile.data.toc
+    mdxOptions: {
+      ...plugins,
+    },
+  };
 
   if (!content) {
     return notFound();
   }
 
   return (
-    <article className="prose ">
-      <h1>{metadata?.title}</h1>
-      <MDXRemote source={content} />
+    <article className="prose px-2 py-10">
+      <MDXRemote
+        source={content}
+        options={options}
+        components={{
+          AsideNote,
+        }}
+      />
     </article>
   );
 }
