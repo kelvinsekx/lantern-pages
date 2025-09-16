e8c0861e43449331a0fb6dee02d587fee8159134
kelvinsekx <utdkelvin@gmail.com>
2025-09-16 07:53:05 +0100

    adjust designs

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,22 +4,20 @@ export const NavigationTop = ({ fixed = false }: { fixed?: boolean }) => {
   return (
     <nav
       id="nav__top"
-      className={`px-2 md:px-10 grid my-auto h-15 bg-muted-100 border-b border-muted-200 w-full shadow-black-200 ${
+      className={`px-2 md:px-10 grid my-auto h-12 bg-muted-100 border-b border-muted-200 w-full shadow-black-200 ${
         fixed && "fixed top-0 z-50"
       }`}
     >
       {/* <h2 className="sr-only">Navigation Menu</h2> */}
-      <div className="flex justify-between items-center  place-self-center w-full">
+      <div className="text-black flex justify-between items-center place-self-center w-full">
         <div>
           <Link href="/">
-            <span className="text-xl md:text-3xl tracking-wide font-semibold text-black-350">
-              Latern Pages
-            </span>
+            <b className="text-xl tracking-widest">Lantern Pages</b>
           </Link>
         </div>
         <div>
-          <button className="bg-red-350 text-muted-0 font-medium rounded-md px-4 py-1">
-            sign in
+          <button className="text-black-250 font-medium rounded-md px-4 py-1 cursor-pointer">
+            Sign in
           </button>
         </div>
       </div>
