5d04639cad831a86fe12cbf1fb2209a79ed140ac
kelvinsekx <utdkelvin@gmail.com>
2026-01-05 21:10:43 +0100

    update config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ import createMdx from "@next/mdx";
 const nextConfigOptions: NextConfig = {
   /* config options here */
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx", ".md"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+    ],
+  },
 };
 
 const withMDX = createMdx({
